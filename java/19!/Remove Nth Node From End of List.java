571cacdb794eb1bc5a05c8412d79ffdd195325ed
charlienzw <charlienzw@gmail.com>
2019-06-24 22:23:11 -0700

    6.24

--- java/19!/Remove Nth Node From End of List.java
+++ java/19!/Remove Nth Node From End of List.java
@@ -1,9 +1,12 @@
 /*
-Given linked list: 1->2->3->4->5, and n = 2.
-
-fast = 3
-
-slow = 3
+    Remove the n-th node from the end of list and return its head
+        Remove one node from List
+            Get the previous node of current node
+                Get the n - 1 node
+                    Set fast pointer to n + 1 node and set slow pointer to head then move them at the same time until fast is null
+            Modify the next pointer of previous node to next node
+            If we need to remove the first node, add a dummy head node before that to make it same with other nodes
+        
 */
 
 /**
@@ -18,22 +21,16 @@ class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        
         ListNode fast = dummy;
-        for(int i = 0; i < n + 1; i++)
-        {
+        for (int i = 0; i < n + 1; i++) {
             fast = fast.next;
         }
-        
         ListNode slow = dummy;
-        while(fast != null)
-        {
-            fast = fast.next;
+        while (fast != null) {
             slow = slow.next;
+            fast = fast.next;
         }
-        
         slow.next = slow.next.next;
-        
         return dummy.next;
     }
 }
--- java/234/Palindrome Linked List.java
+++ java/234/Palindrome Linked List.java
@@ -1,3 +1,13 @@
+/*
+    Determine if it is a palindrome
+        Find the mid node
+            Double pointer to get the mid node
+            Corner case: odd or even number of nodes
+                Count the nodes
+        Reverse the right half list
+        Compare each nodes from head and the reversed right half list
+*/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,52 +18,47 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null) return true;
-        int cnt = 0;
+        if (head == null || head.next == null) return true;
         ListNode cur = head;
-        while(cur != null)
-        {
+        int cnt = 0;
+        while (cur != null) {
             cur = cur.next;
             cnt++;
         }
         
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next != null)
-        {
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
-        ListNode rightpre = cnt % 2 == 0 ? slow : slow.next;
-        ListNode rightreverse = reverse(rightpre);
-        rightpre.next = null;
+        ListNode rightPrev = cnt % 2 == 0 ? slow : slow.next;
         
-        ListNode leftcur = head, rightcur = rightreverse;
-        while(rightcur != null)
-        {
-            if(leftcur.val != rightcur.val)
-            {
+        ListNode reversedRight = reverse(rightPrev);
+        rightPrev.next = null;
+        
+        ListNode leftCur = head, rightCur = reversedRight;
+        while (rightCur != null) {
+            if (leftCur.val != rightCur.val) {
                 return false;
             }
-            leftcur = leftcur.next;
-            rightcur = rightcur.next;
+            leftCur = leftCur.next;
+            rightCur = rightCur.next;
         }
         return true;
     }
     
-    public ListNode reverse(ListNode head)
-    {
-        if(head == null || head.next == null) return head;
-        ListNode pre = head, cur = head.next;
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = head, cur = head.next;
         ListNode next;
-        while(cur != null)
-        {
+        while (cur != null) {
             next = cur.next;
-            cur.next = pre;
-            pre = cur;
+            cur.next = prev;
+            prev = cur;
             cur = next;
         }
         head.next = null;
-        return pre;
+        return prev;
     }
 }
