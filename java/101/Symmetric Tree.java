ded00545b31a9a1f2ccd25c5dfd78d2b3ca43ff0
ningziwe <ningziwe@amazon.com>
2019-07-06 14:41:32 -0700

    lv2

--- java/101/Symmetric Tree.java
+++ java/101/Symmetric Tree.java
@@ -1,4 +1,3 @@
-// recursively
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -10,63 +9,12 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null) return true;
-        return helper(root.left, root.right);
+        return helper(root, root);
     }
-    public boolean helper(TreeNode root1,TreeNode root2)
-    {
-        if(root1 == null&&root2 != null) return false;
-        if(root1 != null&&root2 == null) return false;
-        if(root1 != null&&root2 != null)
-        {
-            if(root1.val != root2.val) return false;
-            return helper(root1.left, root2.right) && helper(root1.right, root2.left);
-        }    
-        return true;
-    }
-}
-
-
-
-// iteratively
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        Stack<TreeNode> s1=new Stack<>();
-        Stack<TreeNode> s2=new Stack<>();
-        TreeNode root1=root,root2=root;
-        while(s1.size()>0&&s2.size()>0||root1!=null&&root2!=null)
-        {
-            if(root1==null&&root2==null)
-            {
-                root1=s1.pop();
-                root2=s2.pop();
-                root1=root1.right;
-                root2=root2.left;
-            }
-            else
-            {
-                if(root1==null&&root2!=null) return false;
-                if(root1!=null&&root2==null) return false;
-                if(root1!=null&&root2!=null)
-                {
-                    if(root1.val!=root2.val)
-                        return false;
-                }
-                s1.push(root1);
-                s2.push(root2);
-                root1=root1.left;
-                root2=root2.right;
-            }
-        }
-        return true;
+    
+    private boolean helper(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return p.val == q.val && helper(p.right, q.left) && helper(p.left, q.right);
     }
 }
